e962ba036c8d4c2446744f458cdc05b1e7c2aecc
Muharani Nabilla Rianti <muharani.nabilla.r@mail.ugm.ac.id>
2015-09-21 12:09:54 +0700

    add xml.php file

--- xml.php
+++ xml.php
@@ -1,13 +1,13 @@
 <?php
 header("Content-type:text/xml");
-mysql_connect("127.0.0.1","root","");
+mysql_connect("localhost","root","");
 $result=mysql("bab2","select LastName,FirstName from Employees order by LastName,FirstName");
 
 $i=0;
 echo'<data_mahasiswa>';
 while ($i < mysql_numrows($result)){
-			$field=mysql_fetch_row($result);
-			echo"<nama>$field[1]$fields[0]</nama>\r\n";
+			$fields=mysql_fetch_row($result);
+			echo"<nama>$fields[1] $fields[0]</nama>\r\n";
 			$i++;
 }
 echo'</data_mahasiswa>';
